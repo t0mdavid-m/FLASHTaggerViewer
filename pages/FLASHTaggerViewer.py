7205058e3fbce4fcf4b8fc892221bd1e47913fe8
Tom David Mueller <tom.mueller@beachouse.de>
2024-09-17 12:33:17 +0200

    allow empty modification output

--- pages/FLASHTaggerViewer.py
+++ pages/FLASHTaggerViewer.py
@@ -108,8 +108,8 @@ def sendDataToJS(selected_data, layout_info_per_exp, grid_key='flash_viewer_grid
 
         if row['ModCount'] > 0:
             mod_masses = [float(m) for m in str(row['ModMass']).split(';')]
-            mod_starts = [int(s) for s in str(row['ModStart']).split(';')]
-            mod_ends = [int(s) for s in str(row['ModEnd']).split(';')]
+            mod_starts = [int(float(s)) for s in str(row['ModStart']).split(';')]
+            mod_ends = [int(float(s)) for s in str(row['ModEnd']).split(';')]
             mod_labels = [s[:-1].replace(',', '; ') for s in str(row['ModID']).split(';')]
         else:
             mod_masses = []
