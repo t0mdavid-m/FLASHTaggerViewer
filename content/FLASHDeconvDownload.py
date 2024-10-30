c1d7f8769a10d21792c8cb0f677397ee05c20c84
Tom David Mueller <tom.mueller@beachouse.de>
2024-10-30 16:23:23 +0100

    update download section with template

--- content/FLASHDeconvDownload.py
+++ content/FLASHDeconvDownload.py
@@ -1,58 +1,74 @@
 import streamlit as st
 
-from os.path import join, exists, dirname, isfile, basename
-from os import listdir
+from pathlib import Path
+import shutil
 
 from src.common.common import page_setup
-from io import StringIO, BytesIO
 from zipfile import ZipFile, ZIP_DEFLATED
 
+page_setup()
 
-page_setup("TaggerViewer")
+# Define output folder here; all subfolders will be handled as downloadable
+# directories
+output_folder = 'FLASHDeconvOutput'
 
-dirpath = join(st.session_state["workspace"], 'FLASHDeconvOutput')
 
-if exists(dirpath):
+# Generate full path
+dirpath = Path(st.session_state["workspace"], output_folder)
+
+# Detect downloadable content
+if dirpath.exists():
     directories = sorted(
-        [entry for entry in listdir(dirpath) if not isfile(entry)]
+        [entry for entry in dirpath.iterdir() if not entry.is_file()]
     )
 else:
     directories = []
 
+# Show error if no content is available for download
 if len(directories) == 0:
     st.error('No results to show yet. Please run a workflow first!')
-    st.stop()
+else:
+    # Table Header
+    columns = st.columns(3)
+    columns[0].write('**Run**')
+    columns[1].write('**Download**')
+    columns[2].write('**Delete Result Set**')
 
-# Table Header
-columns = st.columns((0.4, 0.6))
-columns[0].write('**Run**')
-columns[1].write('**Download**')
-
-# Table Body
-for i, directory in enumerate(directories):
-    st.divider()
-    columns = st.columns((0.4, 0.6))
-    columns[0].empty().write(directory)
-    
-    with columns[1]:
-        button_placeholder = st.empty()
+    # Table Body
+    for i, directory in enumerate(directories):
+        st.divider()
+        columns = st.columns(3)
+        columns[0].empty().write(directory.name)
         
-        clicked = button_placeholder.button('Prepare Download', key=i)
-        if clicked:
-            button_placeholder.empty()
-            with st.spinner():
-                out_zip = join(dirpath, directory, 'output.zip')
-                if not exists(out_zip):
-                    with ZipFile(out_zip, 'w', ZIP_DEFLATED) as zip_file:
-                        for output in listdir(join(dirpath, directory)):
-                            try:
-                                with open(join(dirpath, directory, output), 'r') as f:
-                                    zip_file.writestr(basename(output), f.read())
-                            except:
-                                continue
-                with open(out_zip, 'rb') as f:
-                    button_placeholder.download_button(
-                        "Download ⬇️", f, 
-                        file_name = f'{directory}.zip'
-                    )
+        with columns[1]:
+            button_placeholder = st.empty()
+            
+            # Show placeholder button before download is prepared
+            clicked = button_placeholder.button('Prepare Download', key=i, use_container_width=True)
+            if clicked:
+                button_placeholder.empty()
+                with st.spinner():
+                    # Create ZIP file
+                    out_zip = Path(directory, 'output.zip')
+                    if not out_zip.exists():
+                        with ZipFile(out_zip, 'w', ZIP_DEFLATED) as zip_file:
+                            for output in Path(directory).iterdir():
+                                if output.name == 'output.zip':
+                                    continue
+                                try:
+                                    with open(output, 'r') as f:
+                                        zip_file.writestr(output.name, f.read())
+                                except:
+                                    continue
+                    # Show download button after ZIP file was created
+                    with open(out_zip, 'rb') as f:
+                        button_placeholder.download_button(
+                            "Download ⬇️", f, 
+                            file_name = f'{directory.name}.zip',
+                            use_container_width=True
+                        )
 
+        with columns[2]:
+            if st.button(f"🗑️ {directory.name}", use_container_width=True):
+                shutil.rmtree(directory)
+                st.rerun()
