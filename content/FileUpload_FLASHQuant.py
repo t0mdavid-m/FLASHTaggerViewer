6980503b8cf16e86bb5140cdbeae28d733c4c92c
JeeH-K <jeehyeong91@gmail.com>
2024-11-05 21:40:14 +0100

    update FLASHQuant input files

--- content/FileUpload_FLASHQuant.py
+++ content/FileUpload_FLASHQuant.py
@@ -7,13 +7,17 @@ from src.flashquant import parseFLASHQuantOutput
 from src.common.common import v_space, page_setup, reset_directory, save_params
 from src.flashquant import connectTraceWithResult
 
+# Define input and parsed file types for both input files
+input_file_types = ["quant-files", "trace-files"]
+parsed_df_types = ["quant_dfs", "trace_dfs"]
+tool = 'FLASHQuantViewer'
+
 
 @st.cache_data
-def getUploadedFileDF(quant_files, trace_files, resolution_files):
+def getUploadedFileDF(quant_files, trace_files):
     # leave only names
     quant_files = [Path(f).name for f in quant_files]
     trace_files = [Path(f).name for f in trace_files]
-    resolution_files = [Path(f).name for f in resolution_files]
 
     # getting experiment name from annotated file (tsv files can be multiple per experiment)
     experiment_names = [f[0: f.rfind('.fq')] for f in quant_files]
@@ -21,9 +25,6 @@ def getUploadedFileDF(quant_files, trace_files, resolution_files):
     df = pd.DataFrame({'Experiment Name': experiment_names,
                        'Quant result Files': quant_files,
                        'Mass trace Files': trace_files})
-    if resolution_files:
-        df['Conflict resolution Files'] = resolution_files
-
     return df
 
 
@@ -39,8 +40,8 @@ def remove_selected_experiment_files(to_remove: list[str], params: dict) -> dict
         dict: parameters with updated mzML files
     """
     for input_type, df_type, file_postfix in zip(input_file_types, parsed_df_types,
-                                                 ['.fq.tsv', '.fq.mts.tsv', '.fq_shared.tsv']):
-        input_type_dir = Path(st.session_state["workspace"], input_type)
+                                                 ['.fq.tsv', '.fq.mts.tsv']):
+        input_type_dir = Path(st.session_state["workspace"], tool, input_type)
         # remove all given files from mzML workspace directory and selected files
         for exp_name in to_remove:
             file_name = exp_name + file_postfix
@@ -66,12 +67,10 @@ def handleInputFiles(uploaded_files):
             session_name = 'quant-files'
         elif file.name.endswith('fq.mts.tsv'):
             session_name = 'trace-files'
-        elif file.name.endswith('fq_shared.tsv'):
-            session_name = 'conflict-resolution-files'
 
         if file.name not in st.session_state[session_name]:
             with open(
-                    Path(st.session_state["workspace"], session_name, file.name), "wb"
+                    Path(st.session_state["workspace"], tool, session_name, file.name), "wb"
             ) as f:
                 f.write(file.getbuffer())
             st.session_state[session_name].append(file.name)
@@ -81,11 +80,9 @@ def parseUploadedFiles():
     # get newly uploaded files
     quant_files = st.session_state['quant-files']
     trace_files = st.session_state['trace-files']
-    resolution_files = st.session_state['conflict-resolution-files']
 
     new_quant_files = [f for f in quant_files if f not in st.session_state['quant_dfs']]
     new_trace_files = [f for f in trace_files if f not in st.session_state['trace_dfs']]
-    new_resolution_files = [f for f in resolution_files if f not in st.session_state['conflict_resolution_dfs']]
 
     # if newly uploaded files are not as needed
     if len(new_quant_files) == 0 and len(new_trace_files) == 0:  # if no newly uploaded files, move on
@@ -96,43 +93,21 @@ def parseUploadedFiles():
         for i in not_parsed:
             st.markdown("- " + i)
         return
-    elif (len(new_resolution_files) > 0) & (len(new_quant_files) != len(new_resolution_files)):
-        st.error('Added files (including conflict resolution) are not in pair, so not parsed. \n Here are uploaded ones, but not parsed ones:')
-        not_parsed = new_quant_files + new_trace_files + new_resolution_files
-        for i in not_parsed:
-            st.markdown("- " + i)
-        return
 
     # parse newly uploaded files
-    new_deconv_files = sorted(new_quant_files)
-    new_anno_files = sorted(new_trace_files)
-    if new_resolution_files:
-        new_resolution_files = sorted(new_resolution_files)
-    parsingWithProgressBar(new_deconv_files, new_anno_files, new_resolution_files)
-
-
-def parsingWithProgressBar(infiles_quant, infiles_trace, infiles_resolution):
+    infiles_quant = sorted(new_quant_files)
+    infiles_trace = sorted(new_trace_files)
     with st.session_state['progress_bar_space']:
-        if not infiles_resolution:
-            infiles_resolution = [''] * len(infiles_quant)
-        for quant_f, trace_f, resolution_f in zip(infiles_quant, infiles_trace, infiles_resolution):
+        for quant_f, trace_f in zip(infiles_quant, infiles_trace):
             if not quant_f.endswith('.tsv'):
                 continue
             exp_name = quant_f[0: quant_f.rfind('.fq')]
 
             with st.spinner('Parsing the experiment %s...' % exp_name):
-                if resolution_f:
-                    quant_df, trace_df, resolution_df = parseFLASHQuantOutput(
-                        Path(st.session_state["workspace"], tool, "quant-files", quant_f),
-                        Path(st.session_state["workspace"], tool, "trace-files", trace_f),
-                        Path(st.session_state["workspace"], tool, "conflict-resolution-files", resolution_f),
-                    )
-                    st.session_state['conflict_resolution_dfs'][resolution_f] = resolution_df
-                else:
-                    quant_df, trace_df, _ = parseFLASHQuantOutput(
-                        Path(st.session_state["workspace"], "quant-files", quant_f),
-                        Path(st.session_state["workspace"], "trace-files", trace_f),
-                    )
+                quant_df, trace_df = parseFLASHQuantOutput(
+                    Path(st.session_state.workspace, tool, "quant-files", quant_f),
+                    Path(st.session_state.workspace, tool, "trace-files", trace_f),
+                )
                 st.session_state['quant_dfs'][quant_f] = connectTraceWithResult(quant_df, trace_df)
                 st.session_state['trace_dfs'][trace_f] = []  # need key name, so saving only empty array
             st.success('Done parsing the experiment: %s!' % exp_name)
@@ -160,9 +135,6 @@ def initializeWorkspace(input_file_types_: list, parsed_df_types_: list) -> None
 params = page_setup()
 
 # make directory to store files & initialize data storage
-input_file_types = ["quant-files", "trace-files", "conflict-resolution-files"]
-parsed_df_types = ["quant_dfs", "trace_dfs", "conflict_resolution_dfs"]
-tool = 'FLASHQuantViewer'
 initializeWorkspace(input_file_types, parsed_df_types)
 
 st.title("File Upload")
@@ -175,14 +147,14 @@ with tabs[1]:
     _, c2, _ = st.columns(3)
     if c2.button("Load Example Data", type="primary"):
         # loading and copying example files into default workspace
-        for filetype, session_name in zip(['*fq.tsv', '*fq.mts.tsv', '*fq_shared.tsv'],
+        for filetype, session_name in zip(['*fq.tsv', '*fq.mts.tsv'],
                                           input_file_types):
             for file in Path("example-data/flashquant").glob(filetype):
                 if file.name not in st.session_state[session_name]:
                     shutil.copy(file, Path(st.session_state["workspace"], tool, session_name, file.name))
                     st.session_state[session_name].append(file.name)
         # parsing the example files is done in parseUploadedFiles later
-        st.success("Example mzML files loaded!")
+        st.success("Example tsv files loaded!")
 
 # Upload files via upload widget
 with tabs[0]:
@@ -200,17 +172,15 @@ with tabs[0]:
     Select data for analysis from the uploaded files shown below.
     
     **💡 Make sure that the same number of FLASHQuant result files (\*fq.tsv and \*fq.mts.tsv) are uploaded!**
-    
-    **💡 To visualize conflict resolution, \*fq_shared.tsv files should be uploaded**
     """
     )
     with st.form('files_uploader_form', clear_on_submit=True):
         uploaded_file = st.file_uploader(
-            "FLASHQuant output files", accept_multiple_files=True
+            "FLASHQuant output files", accept_multiple_files=True, type=["tsv"]
         )
         _, c2, _ = st.columns(3)
         # User needs to click button to upload selected files
-        if c2.form_submit_button("Add the tsv files", type="primary"):
+        if c2.form_submit_button("Add files to workspace", type="primary"):
             # Copy uploaded files to *-files directory
             if uploaded_file:
                 # A list of files is required, since online allows only single upload, create a list
@@ -230,7 +200,6 @@ parseUploadedFiles()
 # for error message or list of uploaded files
 quant_files = sorted(st.session_state["quant_dfs"].keys())
 trace_files = sorted(st.session_state["trace_dfs"].keys())
-res_files = sorted(st.session_state["conflict_resolution_dfs"].keys())
 
 # error message if files not exist
 if len(quant_files) == 0 and len(trace_files) == 0:
@@ -243,7 +212,7 @@ elif len(quant_files) != len(trace_files):
     st.error("The same number of quant result and trace tsv file should be uploaded!")
 else:
     v_space(2)
-    st.session_state["quant-experiment-df"] = getUploadedFileDF(quant_files, trace_files, res_files)
+    st.session_state["quant-experiment-df"] = getUploadedFileDF(quant_files, trace_files)
     st.markdown('**Uploaded experiments in current workspace**')
     st.dataframe(st.session_state["quant-experiment-df"])  # show table
     v_space(1)
@@ -263,7 +232,7 @@ else:
         if c1.button("⚠️ Remove **all**", disabled=not any(st.session_state["quant-experiment-df"])):
             for file_option, df_option in zip(input_file_types, parsed_df_types):
                 if file_option in st.session_state:
-                    reset_directory(Path(st.session_state.workspace, file_option))
+                    reset_directory(Path(st.session_state.workspace, tool, file_option))
                     st.session_state[file_option] = []
                 if df_option in st.session_state:
                     st.session_state[df_option] = {}
