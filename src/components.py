8b87d399424453595f2816b19b174e58ce1b7e90
Tom David Mueller <tom.mueller@beachouse.de>
2024-11-08 14:30:52 +0100

    update vue

--- src/components.py
+++ src/components.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as st_components
 
 # Create a _RELEASE constant. We'll set this to False while we're developing
 # the component, and True when we're ready to package and distribute it.
-_RELEASE = False
+_RELEASE = True
 
 
 def flash_viewer_grid_component(components, data, component_key='flash_viewer_grid'):
